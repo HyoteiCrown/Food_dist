f72e73e5eed2abaf11a8aaa7d5ff3be8e110ce1a
Hyotei <wrwaster228@gmail.com>
2024-12-07 11:58:48 +0300

    ver

---
+++ js/script.js
@@ -0,0 +1,290 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tabheader__item");
+  const tabsContent = document.querySelectorAll(".tabcontent");
+  const tabsParent = document.querySelector(".tabheader__items");
+
+  function hideTabContent(item) {
+    tabsContent.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].style.display = "block";
+    tabs[i].classList.add("tabheader__item_active");
+  }
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, index) => {
+        if (item === target) {
+          hideTabContent();
+          showTabContent(index);
+        }
+      });
+    }
+  });
+
+  //Timer
+
+  const deadline = "2024-12-31";
+
+  function getTimerRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return "0" + num;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector("#days");
+    const hours = timer.querySelector("#hours");
+    const minutes = timer.querySelector("#minutes");
+    const seconds = timer.querySelector("#seconds");
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimerRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(".timer", deadline);
+
+  //modal
+
+  const modalTrigger = document.querySelectorAll("[data-modal]");
+  const modal = document.querySelector(".modal");
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerID);
+  }
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
+
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+  }
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal || e.target.getAttribute("data-close") == "") {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  const modalTimerID = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (
+      window.scrollY + document.documentElement.clientHeight >
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
+
+  //card classes
+
+  class MenuCard {
+    constructor(name, descr, price, img, alt, parentSelector, ...classes) {
+      this.name = name;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.img = img;
+      this.parentSelector = document.querySelector(parentSelector);
+      this.alt = alt;
+      this.transfer = 98;
+      this.changeToRus();
+    }
+    changeToRus() {
+      this.price = +this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+      element.innerHTML = `
+    <img src=${this.img} alt=${this.alt}>
+    <h3 class="menu__item-subtitle">${this.name}</h3>
+    <div class="menu__item-descr">${this.descr}</div>
+    <div class="menu__item-divider"></div>
+    <div class="menu__item-price">
+        <div class="menu__item-cost">Цена:</div>
+        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+    </div>
+    `;
+      this.parentSelector.append(element);
+    }
+  }
+
+  new MenuCard(
+    "Меню Фитнес",
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    "img/tabs/vegy.jpg",
+    "vegy",
+    ".menu .container"
+  ).render();
+
+  new MenuCard(
+    "Меню Премиум",
+    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+    15,
+    "img/tabs/elite.jpg",
+    "elite",
+    ".menu .container"
+  ).render();
+
+  new MenuCard(
+    "Меню Постное",
+    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+    11,
+    "img/tabs/post.jpg",
+    "post",
+    ".menu .container"
+  ).render();
+
+  //forms
+
+  const forms = document.querySelectorAll("form");
+
+  const message = {
+    loading: "img/form/spinner.svg",
+    success: "Спасибо! Мы скоро свяжемся с вами.",
+    failure: "Что-то пошло не так...",
+  };
+
+  forms.forEach((item) => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+  
+      const statusMessage = document.createElement("img");
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText=`
+        display: block;
+        margin: 0 auto;
+      `
+      form.insertAdjacentElement('afterend', statusMessage);
+  
+        
+      
+      const formData = new FormData(form);
+  
+      const object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+  
+
+      fetch('server.php', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body:JSON.stringify(object)
+        }).then(data=>data.text())
+        .then(data =>{
+          console.log(data);
+          showThanksModal(message.success);
+          statusMessage.remove();
+        }).catch(() => {
+          showThanksModal(message.failure);
+        }).finally(() => {
+          form.reset();
+        })
+  });
+  }
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector(".modal__dialog");
+    prevModalDialog.classList.add("hide");
+  
+    const modalDialog = document.createElement("div");
+    modalDialog.classList.add("modal__dialog");
+    modalDialog.classList.add("show");
+    modalDialog.classList.add("modal__dialog--visible");
+    modalDialog.innerHTML = `
+      <div class="modal__content">
+          <div data-close class="modal__close">&times;</div>
+          <div class="modal__title">${message}</div>
+      </div>
+    `;
+  
+    document.querySelector(".modal").append(modalDialog);
+  
+    setTimeout(() => {
+      modalDialog.remove();
+      prevModalDialog.classList.add("show");
+      prevModalDialog.classList.remove("hide");
+      closeModal();
+    }, 4000);
+  };
+});
+
+
+
+
+
+
+
---
+++ js/script2.js
@@ -0,0 +1,88 @@
+"use strict";
+
+// const names = ['Mike', 'John', 'Ann','HyoteiCrown'];
+
+// const shortNames = names.filter((name) => {
+//     return name.length < 4;
+// });
+
+// console.log(shortNames);
+
+// const answers =['LiKe', 'ann', 'HyoteiCrown', 'Mike'];
+
+// const result = answers.map(item => item.toLowerCase());
+
+// console.log(result);
+
+// const peren = [4, 10, 5, 6, 7, 8];
+
+// console.log(peren.every(item => typeof item === 'number'));
+
+// const arr =[2,5,7,20,11,6];
+
+// console.log(arr.reduce((item, next) => item + next));
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// const uniteFruits = fruits.reduce((item, next) => `${item}, ${next}`);
+// console.log(uniteFruits);
+
+// const object = {
+//     husband:'human',
+//     ann:'human',
+//     eva:'animal',
+//     dog:'animal'
+// };
+
+// const newArray = Object.entries(object)
+// .filter(item => item[1] === 'human')
+// .map(item => item[0]);
+// console.log(newArray);
+// const telephoneBook = {
+//   John: "123456",
+//   Ann: "987654",
+//   Peter: "567890",
+//   Alex: "111111",
+//   Jack: "222222",
+//   Chad: "333333",
+//   HyoteiCrown: "444444",
+// };
+
+// //binary search
+
+// function binarySearch(arr, val) {
+//   let min = 0;
+//   let max = arr.length - 1;
+//   let middle;
+//   let found = false;
+//   let count = 0;
+
+//   while (min <= max && !found) {
+//     middle = Math.floor((min + max) / 2);
+//     if (arr[middle] === val) {
+//       found = true;
+//     } else if (arr[middle] < val) {
+//       min = middle + 1;
+//     } else {
+//       max = middle - 1;
+//     }
+//     count++;
+//   }
+//   return count;
+// }
+// let mapList =[
+//     {name: 'John', age: 25},
+//     {name: 'Ann', age: 30},
+//     {name: 'Peter', age: 35},
+//     {name: 'Alex', age: 20},
+//     {name: 'Jack', age: 40},
+//     {name: 'Chad', age: 45},
+//     {name: 'HyoteiCrown', age: 50}
+// ]
+
+// function newMapList() = {
+//     return mapList.filter(item => item.age > 30).map(item => item.name)
+    
+// }
+
+
