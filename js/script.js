361344e90bd59b71cfc44a183037d228163cd1ac
Hyotei <wrwaster228@gmail.com>
2024-12-10 23:01:27 +0300

    animated slider v1

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll(".tabheader__item");
   const tabsContent = document.querySelectorAll(".tabcontent");
   const tabsParent = document.querySelector(".tabheader__items");
+  const slides = document.querySelectorAll(".offer__slide");
+  const prevButton = document.querySelector(".offer__slider-prev");
+  const nextButton = document.querySelector(".offer__slider-next");
+  const currentSlide = document.getElementById("current");
+  const totalSlides = document.getElementById("total");
 
   function hideTabContent(item) {
     tabsContent.forEach((item) => {
@@ -319,4 +324,45 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("http://localhost:3000/menu")
     .then((data) => data.json())
     .then((res) => console.log(res));
-});
+
+  //slider
+
+  let slideIndex = 1;
+
+  showSlides(slideIndex);
+
+  if(slides.length > 10) {
+    totalSlides.textContent = `0${slides.length}`;
+  } else {
+    totalSlides.textContent = slides.length;
+  }
+
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => (item.style.display = "none"));
+
+    slides[slideIndex - 1].style.display = "block";
+
+    if (slides.length < 10) {
+      currentSlide.textContent = `0${slideIndex}`;
+      } else {
+      currentSlide.textContent = slideIndex;
+      }
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  prevButton.addEventListener("click", () => plusSlides(-1));
+
+  nextButton.addEventListener("click", () => plusSlides(1));
+
+}); 
